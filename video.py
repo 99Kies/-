a8bd7a953b13cd1cd3c6ecb46dcbab56849aee5a
99Kies <1290017556@qq.com>
2019-07-15 11:19:44 +0800

    del

--- video.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('movies.csv','r',encoding='gbk') as csvfile:
-    reader = csv.reader(csvfile)
-    print(type(reader))
-    print(reader)
-    for row in reader:
-        print(row)
